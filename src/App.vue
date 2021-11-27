1f5dfb65efbc60f7df98821d78b6ae83e9587b8c
hoshianaaa <hoshina12345@gmail.com>
2021-11-27 17:45:55 +0900

    rosとのconnectionができた!

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,36 @@
 </template>
 
 <script>
+import ROSLIB from "roslib" // 追加
 import HelloWorld from './components/HelloWorld.vue'
+
+const ros =  new ROSLIB.Ros({
+    url: 'ws://localhost:9090'
+});
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+
+  // *** 追加 ここから *** //
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init: function () {
+      ros.on("connection", function() {
+        console.log("connected to websocket server.");
+      });
+      ros.on("error", function(error) {
+        console.log("error connecting to websocket server: ", error);
+      });
+      ros.on("close", function() {
+        console.log("connection to websocket server closed.");
+      });
+    }
+    // *** 追加 ここまで *** //
   }
 }
 </script>
